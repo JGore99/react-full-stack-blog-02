60128decd66fce983944d5129b352919b6262d68
Jeff_Gore <JefGore@gmail.com>
2022-02-03 15:26:30 -0500

    add blog model

---
+++ models/blog.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose'
+
+const commentSchema = new mongoose.Schema({
+  content: String,
+  author: {type: mongoose.Schema.Types.ObjectId, ref: "Profile"},
+},{
+  timestamps: true
+})
+
+
+const blogSchema = new mongoose.Schema({
+    title: String,
+    content: String,
+    author: {type: mongoose.Schema.Types.ObjectId, ref: "Profile"},
+    comments: [commentSchema]
+  }, {
+    timestamps: true,
+  })
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+export {
+  Blog
+}
